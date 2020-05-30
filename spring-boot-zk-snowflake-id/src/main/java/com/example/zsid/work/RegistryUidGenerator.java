d856b90e36889f2becca96c34c2c530ae44f78f7
ChinaShuai <11193866572@qq.com>
2020-05-30 11:57:00 +0800

    delete code

--- spring-boot-zk-snowflake-id/src/main/java/com/example/zsid/work/RegistryUidGenerator.java
+++ spring-boot-zk-snowflake-id/src/main/java/com/example/zsid/work/RegistryUidGenerator.java
@@ -9,42 +9,19 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-@ConditionalOnBean(RegistryUidGenerator.class)
 @Component
-public class RegistryUidGenerator implements ApplicationContextAware {
-
+public class RegistryUidGenerator {
     private static final Logger LOG = LoggerFactory.getLogger(RegistryUidGenerator.class);
 
     @Autowired
     private DisposableWorkerIdAssigner disposableWorkerIdAssigner;
-
-    private static ApplicationContext context;
-
-    /**
-     *
-     */
-    private RegistryUidGenerator() {
-
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        context = applicationContext;
-    }
 
     private static ConcurrentHashMap<String, UidGenerator> MAP = new ConcurrentHashMap<>();
 
@@ -78,26 +55,10 @@ public class RegistryUidGenerator implements ApplicationContextAware {
     /**
      * 指定bean name 刷新 bean，并缓存到map
      *
-     * @param beanName
+     * @param beanName domain的bean name
      */
     public void refreshBean(String beanName) {
         Preconditions.checkState(DOMAIN_BEAN_NAME_SET.contains(beanName), "beanName = %s 没有配置唯一ID", beanName);
-//
-//        // Bean的实例工厂
-//        DefaultListableBeanFactory dbf = (DefaultListableBeanFactory) context.getAutowireCapableBeanFactory();
-//        // Bean构建 BeanService.class 要创建的Bean的Class对象
-//        BeanDefinitionBuilder dataSourceBuider = BeanDefinitionBuilder.genericBeanDefinition(CachedUidGenerator.class);
-//        // 向里面的属性注入值，提供get set方法
-//        dataSourceBuider.addPropertyValue("boostPower", 3);
-//        dataSourceBuider.addPropertyValue("paddingFactor", 10);
-//        dataSourceBuider.addPropertyValue("workerIdAssigner", context.getBean("disposableWorkerIdAssigner"));
-//        // dataSourceBuider.setParentName(""); 同配置 parent
-//        // 同配置 scope
-//        dataSourceBuider.setScope(ConfigurableBeanFactory.SCOPE_SINGLETON);
-//        // 将实例注册spring容器中 bs 等同于 id配置
-//        dbf.registerBeanDefinition(beanName, dataSourceBuider.getBeanDefinition());
-//        UidGenerator beanService = (CachedUidGenerator) context.getBean(beanName);
-//
         long workId = disposableWorkerIdAssigner.assignWorkerId();
 
         // use CachedUidGenerator
@@ -125,26 +86,15 @@ public class RegistryUidGenerator implements ApplicationContextAware {
         while (REFRESH_FLAG.get()) {
 
         }
-        if (MAP.containsKey(beanName)) {
+        if (MAP.containsKey(beanName) && MAP.get(beanName) != null) {
             return MAP.get(beanName);
-        }
-        UidGenerator beanService = null;
-        try {
-            beanService = (UidGenerator) context.getBean(beanName);
-        } catch (Exception e) {
-            LOG.error("beanName={}不存在，需要创建", beanName, e);
-        }
-        if (beanService != null) {
-            MAP.put(beanName, beanService);
-            return beanService;
         }
         long start = System.currentTimeMillis();
         synchronized (RegistryUidGenerator.class) {
-            if (beanService == null && MAP.get(beanName) == null) {
+            if (!MAP.containsKey(beanName) || MAP.get(beanName) == null) {
                 LOG.warn("beanName={}不存在，需要创建", beanName);
                 refreshBean(beanName);
             }
-
         }
         LOG.warn("generateUidGenerator beanName={}, spend time={}", beanName, (System.currentTimeMillis() - start));
         return MAP.get(beanName);
